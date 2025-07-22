ed6c6cf8acfa49886c0e852b41ff44ac12d57c47
rithisa <rithisa.802439@sxcce.edu.in>
2025-07-22 15:16:14 +0530

    build fix

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,35 +1,35 @@
-export default function Resume(){
-    return(
+export default function Resume() {
+    return (
         <div>
-        <h1 className="text-4xl flex justify-center ">Resume</h1>
-        <h2 className="text-2xl  flex justify-center m-8 ">Work Experience</h2>
-        <div className="text-teft m-50">
-            <ul>
-                
-            <h1 className="text-left">Theater </h1> 
-            <p><p className="text-right">Production Company   |   Project Name   |   2035  |   Lead Actor<br></br>
-              Production Company   |   Project Name   |   2035  |   Dancer<br></br>
-            Production Company   |   Project Name   |   2035  |   Chorus Member</p></p>
+            <h1 className="text-4xl flex justify-center ">Resume</h1>
+            <h2 className="text-2xl  flex justify-center m-8 ">Work Experience</h2>
+            <div className="text-teft m-50">
+                <ul>
 
-            <h2 className="p-2 text-left">Flim</h2>
-            <p className="p-2 text-right">Production Company   |   Project Name   |   2035  |   Lead Actor<br></br>
-              Production Company   |   Project Name   |   2035  |   Understudy<br></br>
-             Production Company   |   Project Name   |   2035  |   Supporting Actor
-</p>
-            <h3 className="p-2">Commercial</h3>
-            <p className="text-right">Production Company   |   Project Name   |   2035  |   Lead Actor<br></br>
-                Production Company   |   Project Name   |   2035  |   Supporting Actor<br></br>
-             Production Company   |   Project Name   |   2035  |   Extra
-</p>
-        
-    
-                
-        
-            </ul>
-        </div>
-        </div>
-        
+                    <h1 className="text-left">Theater </h1>
+                    <p><p className="text-right">Production Company   |   Project Name   |   2035  |   Lead Actor<br></br>
+                        Production Company   |   Project Name   |   2035  |   Dancer<br></br>
+                        Production Company   |   Project Name   |   2035  |   Chorus Member</p></p>
 
-        
+                    <h2 className="p-2 text-left">Flim</h2>
+                    <p className="p-2 text-right">Production Company   |   Project Name   |   2035  |   Lead Actor<br></br>
+                        Production Company   |   Project Name   |   2035  |   Understudy<br></br>
+                        Production Company   |   Project Name   |   2035  |   Supporting Actor
+                    </p>
+                    <h3 className="p-2">Commercial</h3>
+                    <p className="text-right">Production Company   |   Project Name   |   2035  |   Lead Actor<br></br>
+                        Production Company   |   Project Name   |   2035  |   Supporting Actor<br></br>
+                        Production Company   |   Project Name   |   2035  |   Extra
+                    </p>
+
+
+
+
+                </ul>
+            </div>
+        </div>
+
+
+
     )
 }
